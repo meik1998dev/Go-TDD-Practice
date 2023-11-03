076c44787cc0214b74b752fb610c17fdac72746e
meik1998news <mohammad+test@newsifier.com>
2023-11-03 18:01:03 +0300

    extract structs to external file

--- methodsAndStructs/methodsAndStructs.go
+++ methodsAndStructs/methodsAndStructs.go
@@ -4,24 +4,6 @@ import "math"
 
 func main() {}
 
-type Rectangle struct {
-	Width  float64
-	Height float64
-}
-
-type Circle struct {
-	Radius float64
-}
-
-type Triangle struct {
-	Base   float64
-	Height float64
-}
-
-type Shape interface {
-	Area() float64
-}
-
 func (r Rectangle) Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
---
+++ methodsAndStructs/shape_structs.go
@@ -0,0 +1,19 @@
+package main
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
