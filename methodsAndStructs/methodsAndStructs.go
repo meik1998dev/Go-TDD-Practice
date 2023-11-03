28c0fa9276fcd257aa1215bac576a5e3c4d66572
meik1998news <mohammad+test@newsifier.com>
2023-11-03 17:55:20 +0300

    practice methods and structs

---
+++ methodsAndStructs/methodsAndStructs.go
@@ -0,0 +1,39 @@
+package main
+
+import "math"
+
+func main() {}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
